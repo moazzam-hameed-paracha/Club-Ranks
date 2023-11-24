27216c8591fe62716d5a046394d9ba0b3f415d9b
Moazzam Hameed Paracha <moazzam.hameed@minascode.com>
2023-11-24 19:07:54 +0500

    Update DashboardPage.tsx

--- src/components/pages/DashboardPage/DashboardPage.tsx
+++ src/components/pages/DashboardPage/DashboardPage.tsx
@@ -1,7 +1,42 @@
+import { CustomHeader } from "@src/components/common";
 import React from "react";
+import styles from "./styles.module.scss";
+import { Button } from "react-bootstrap";
+// import { signIn, useSession } from "next-auth/react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  // const { data: session } = useSession();
+
+  return (
+    <>
+      <CustomHeader />
+      <section className="p-3">
+        <div className="w-50 mx-auto">
+          <h2 className="text-center text-white">
+            Discover where you belong with EDUNOT!
+          </h2>
+          <hr />
+
+          <div className={styles.cardOuter}>
+            <div className={styles.cardInner}>
+              <h5>Enter your CV/Interests, and see which where you fit!</h5>
+              <h5>
+                Clubs to join, which Professors to research, & what Classes to
+                take!
+              </h5>
+
+              <Button
+                variant="outline-secondary"
+                //  onClick={() => signIn()}
+              >
+                SIGN UP!
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default DashboardPage;
