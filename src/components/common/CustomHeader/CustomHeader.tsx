af9f54c0ab547afabd1cb72da554689031f9e922
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2023-11-25 18:33:32 +0500

    Add useEffect hook to set isLoggedIn state from localStorage

--- src/components/common/CustomHeader/CustomHeader.tsx
+++ src/components/common/CustomHeader/CustomHeader.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { PAGES } from "@src/constants/pages";
+import {cookies} from "next/headers";
 
 function Header() {
-  const isLoggedIn = Boolean(
-    JSON.parse(localStorage.getItem("loggedIn") || "false")
-  );
+  const [isLoggedIn, setLoggedIn] = React.useState(false);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("loggedIn") || "false");
+    setLoggedIn(storedData);
+  }, []);
 
   return (
     <Navbar expand="lg" className={styles.nav}>
@@ -16,7 +20,7 @@ function Header() {
           <Image src="/images/logo.png" alt="" width={75} height={60} />
         </Navbar.Brand>
         {isLoggedIn && (
-          <Nav className="">
+          <Nav className="justify-space-between">
             <NavDropdown title="Rank Lists" id="basic-nav-dropdown">
               <NavDropdown.Item href={PAGES.PROFESSORS}>
                 Professors
@@ -26,6 +30,7 @@ function Header() {
               <NavDropdown.Divider />
               <NavDropdown.Item href={PAGES.CLUBS}>Clubs</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link>Sign Out</Nav.Link>
           </Nav>
         )}
       </Container>
