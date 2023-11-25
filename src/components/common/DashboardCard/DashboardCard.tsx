1767abf11aa1a33a8203c18db0111a41a5b0fb3a
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2023-11-25 20:16:01 +0500

    Add DashboardCard component

---
+++ src/components/common/DashboardCard/DashboardCard.tsx
@@ -0,0 +1,26 @@
+
+
+import React, { ReactElement } from "react";
+import { Card } from "react-bootstrap";
+import { IconType } from "react-icons";
+
+type DashboardCardProps = {
+    title: string;
+    subtitle: string;
+    icon: ReactElement<IconType>;
+
+}
+
+const DashboardCard: React.FC<DashboardCardProps> = ({ title, subtitle, icon }) => {
+    return (
+        <Card className="text-center">
+            <Card.Body>
+                {icon}
+                <Card.Title className="mt-4">{title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default DashboardCard;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export * from "./StarRating";
 export * from "./CustomHeader";
 export * from "./ProfessorBox";
 export * from "./LogoCarousel";
+export * from "./DashboardCard";
